288c247d19da465f35d39945ff6eac6e83e06a8b
David-lp-ops <95448910+DavidLpsl@users.noreply.github.com>
2021-12-02 17:20:51 -0500

    fix:Change message app.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Se realiza un cambio en el codigo fuente!";
 
     public App() {}
 
